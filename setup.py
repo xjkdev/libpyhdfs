5cf03347babafdaaeacb364a7e9004af97bf9940
Joshua Downer <joshua.downer@gmail.com>
2015-06-04 07:15:40 -0400

    libpyhdfs: modified setup.py to upload to pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 from distutils.core import setup, Extension
 
 pyhdfs = Extension('pyhdfs',
-                   sources = ['src/pyhdfs.c'],
-                   include_dirs = ['/usr/lib/jvm/java-6-sun/include/'],
-                   libraries = ['hdfs'],
-                   library_dirs = ['lib'],
-                   runtime_library_dirs = ['/usr/local/lib/pyhdfs', '/usr/lib/jvm/java-6-sun/jre/lib/i386/server'],
-                   )
+        sources=['src/pyhdfs.c'],
+        include_dirs=[
+            'src',
+            '/usr/lib/jvm/java-6-sun/include/',
+            '/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.79-2.5.5.0.fc20.x86_64/include',
+            ],
+        libraries=['hdfs'],
+        library_dirs=['lib'],
+        runtime_library_dirs=[
+            '/usr/local/lib/pyhdfs',
+            '/usr/lib/jvm/java-6-sun/jre/lib/i386/server',
+            '/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.79-2.5.5.0.fc20.x86_64/jre/lib/amd64/server',
+            ],
+        )
 
-setup(name = 'PyHdfs',
-      version = '0.1',
-      author = 'Deng Zhiping',
-      author_email = 'kofreestyler@gmail.com',
-      description = "Python wrapper for libhdfs",
-      long_description = """libpyhdfs is a Python extension module which wraps
-                             the C API in libhdfs to access Hadoop file system""",
-      url = "http://code.google.com/p/libpyhdfs",
-      license = "Apache License 2.0", 
-      platforms = ["GNU/Linux"],
-      ext_modules = [pyhdfs])
+setup(name='python-hdfs',
+        version='0.2',
+        author='Deng Zhiping, Joshua Downer',
+        author_email='joshua.downer@gmail.com',
+        description="Python wrapper for libhdfs",
+        long_description=(open('README').read()),
+        url="https://github.com/jdowner/libpyhdfs",
+        license="Apache License 2.0",
+        platforms=["GNU/Linux"],
+        ext_modules=[pyhdfs],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Operating System :: Unix',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 3',
+            'Topic :: System :: Filesystems',
+            'Topic :: Utilities',
+            ],
+        )
